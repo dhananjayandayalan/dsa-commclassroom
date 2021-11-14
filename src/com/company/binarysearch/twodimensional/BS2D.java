611656c6b81fadd5cb6a2274c0e48a1b42455c39
Dhananjayan D <dhananjayan.dayalan@gmail.com>
2021-11-14 15:42:45 +0530

    2D Binary Search Questions

---
+++ src/com/company/binarysearch/twodimensional/BS2D.java
@@ -0,0 +1,35 @@
+package com.company.binarysearch.twodimensional;
+
+import java.util.Arrays;
+
+public class BS2D {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {28, 29, 37, 49},
+                {33, 34, 38, 50}
+        };
+
+        int target = 45;
+        System.out.println(Arrays.toString(search(matrix, 45)));
+    }
+
+    private static int[] search(int[][] matrix, int target) {
+        int r = 0;
+        int c = matrix.length - 1;
+
+        while(r < matrix.length && c >= 0) {
+            if(matrix[r][c] == target) {
+                return new int[]{r, c};
+            }
+            if(matrix[r][c] < target) {
+                r += 1;
+            } else {
+                c -= 1;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
